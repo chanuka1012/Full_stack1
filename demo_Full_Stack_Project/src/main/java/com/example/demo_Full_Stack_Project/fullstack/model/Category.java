bb97bdebea09d901fb04959a3231346efa7acb98
chanuka1012 <chanukarathnayake1012@gmail.com>
2024-12-19 01:37:41 +0530

    11th commit

---
+++ demo_Full_Stack_Project/src/main/java/com/example/demo_Full_Stack_Project/fullstack/model/Category.java
@@ -0,0 +1,25 @@
+package com.example.demo_Full_Stack_Project.fullstack.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "Category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Category {
+
+    @Id
+    private String id;
+    private String name;
+    //(notes = "Name of the category", example = "fitness")
+
+    private String description;
+    //(notes = "Description of the category", example = "Fitness supplements")
+
+}
