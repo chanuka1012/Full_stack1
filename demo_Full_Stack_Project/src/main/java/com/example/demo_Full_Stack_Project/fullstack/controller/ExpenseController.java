a90422e687fda74c28d282235106477098905ddf
chanuka1012 <chanukarathnayake1012@gmail.com>
2025-01-03 22:46:49 +0530

    expense hande 02

--- demo_Full_Stack_Project/src/main/java/com/example/demo_Full_Stack_Project/fullstack/controller/ExpenseController.java
+++ demo_Full_Stack_Project/src/main/java/com/example/demo_Full_Stack_Project/fullstack/controller/ExpenseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.example.demo_Full_Stack_Project.fullstack.service.ExpenseService;
 
 @RestController
 @RequestMapping("api/expenses")
+@CrossOrigin(origins = "http://localhost:5173") // Allow requests from React app
 
 public class ExpenseController {
 
